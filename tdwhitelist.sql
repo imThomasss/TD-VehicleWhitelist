151e24a30dba2c091bfbeda2982fa1b7214063fd
Tjjones197 <88681751+Tjjones197@users.noreply.github.com>
2024-07-24 02:17:53 +0100

    updated SQL
    removed sample data

--- tdwhitelist.sql
+++ tdwhitelist.sql
@@ -7,9 +7,3 @@ CREATE TABLE IF NOT EXISTS vehicle_permissions (
     UNIQUE KEY (player_id, vehicle_spawn_code)
 );
 
--- Sample data
-INSERT INTO vehicle_permissions (player_id, player_name, player_discord_id, vehicle_spawn_code) VALUES 
-('player_cfx_id1', 'Player 1', 'discord_id_1', 'spawn_code1'),
-('player_cfx_id2', 'Player 2', 'discord_id_2', 'spawn_code1'),
-('player_cfx_id3', 'Player 3', 'discord_id_3', 'spawn_code2'),
-('player_cfx_id4', 'Player 4', 'discord_id_4', 'spawn_code2');
